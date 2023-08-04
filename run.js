eedc412889e66a8b46501da7a379741afa843e47
Anubhav <anubhavsharma313@gmail.com>
2023-08-04 02:29:14 -0700

    initialize

---
+++ run.js
@@ -0,0 +1,91 @@
+function addStyleSheets(styles) {
+    const head = document.querySelector('head');
+
+    for (let style of styles) {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = style;
+        head.append(link);
+    }
+}
+
+function addScripts(scripts) {
+    const body = document.querySelector('head');
+
+    for (let script of scripts) {
+        const scriptEl = document.createElement('script');
+        scriptEl.type = 'text/javascript';
+        scriptEl.src = script
+        body.append(scriptEl);
+    }
+}
+
+async function sleep(s) {
+    return new Promise(resolve => setTimeout(resolve, s * 1000));
+}
+
+async function startLoading(time) {
+    const loadingScreen = document.createElement('div');
+    loadingScreen.style.width = '100%';
+    loadingScreen.style.height = '100%';
+    loadingScreen.style.position = 'absolute';
+    loadingScreen.style.top = '0';
+    loadingScreen.style.left = '0';
+    loadingScreen.style.zIndex = '1001';
+    loadingScreen.style.background = 'black';
+
+    let interval = 0.5,
+        curTime = 0;
+
+    loadingScreen.style.transition = `background ${interval}s`;
+
+    const body = document.querySelector('body');
+    body.appendChild(loadingScreen);
+    body.style.overflow = 'hidden';
+
+    while (curTime < time) {
+        let h, s, l;
+        h = Math.floor(Math.random() * 360);
+        s = Math.floor(Math.random() * 100);
+        l = Math.floor(Math.random() * 100);
+
+        loadingScreen.style.background = `hsl(${h}, ${s}%, ${l}%)`
+        await sleep(interval);
+
+        curTime += interval;
+    }
+}
+
+const scripts = [
+    'https://anubhav-kaushik.github.io/tab-addons/widget-tab-type.js',
+    'https://anubhav-kaushik.github.io/quiz-creator/js/action.js',
+    'https://anubhav-kaushik.github.io/quiz-creator/js/quiz-creator.js',
+    'https://anubhav-kaushik.github.io/marksCalc/tcs-marks-calc.js',
+];
+
+const styleSheets = [
+    'https://anubhav-kaushik.github.io/tab-addons/widget-tab-type.css',
+    'https://anubhav-kaushik.github.io/quiz-creator/css/quiz-style.css',
+    'https://anubhav-kaushik.github.io/marksCalc/style.css',
+    'https://anubhav-kaushik.github.io/create-table/css/style.css',
+]
+
+addStyleSheets(styleSheets);
+addScripts(scripts);
+
+async function run(tier) {
+    await sleep(3);
+    const result = main(page, '.section-cntnr', '.section-lbl', '.rw', markingScheme, tier);
+
+    const mainBody = document.querySelector('body');
+    mainBody.innerHTML = initialTabBlock().outerHTML;
+
+    const finalData = {
+        'Quiz': createQuiz(result['answerKeyDict'], `${result['candidateInfo']['Exam Name']}<br>(${result['candidateInfo']['Exam Time']})`)
+    }
+
+    addTabs(finalData, '.tabs-widget .tabs', '.tabs-widget .tabs-content')
+}
+
+
